a6c0afe1384f424bf085ddd213a5e5a1aa9a6071
Bradigson <bradigson@gmail.com>
2022-04-30 21:26:13 -0400

    emoji added

--- src/alerts/Notificacion.js
+++ src/alerts/Notificacion.js
@@ -2,7 +2,7 @@ import Alert from 'sweetalert2';
 
 const handleNoificacion = ()=>{
     Alert.fire({
-        title : 'Notificación',
+        title : 'Notificación 😱',
         text : 'Examen de los intervalos activado el 29/04/2022 hasta 7/05/2022 ',
         footer : 'Goog Luck! 😎',
         icon : 'info'
